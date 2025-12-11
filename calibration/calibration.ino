9ae1a377efa5d614cef42258fb591670a59a9496
0megq <nvatanshenas55@gmail.com>
2025-12-11 08:02:38 -0800

    more updates

--- calibration/calibration.ino
+++ calibration/calibration.ino
@@ -1,6 +1,6 @@
 
 
-int counts[12] = {1, 10, 20, 30, 40, 50, 0, 0, 0, 0, 0, 0};
+int counts[12] = {20, 30, 40, 50, 60, 70, 0, 0, 0, 0, 0, 0};
 
 float startTime;
 
@@ -11,11 +11,11 @@ void setup() {
   pinMode(A0, INPUT);
 
   //  Set prescaler to 8 example (can be adjusted)
-  ADC->CTRLA.bit.ENABLE = 0;
-  while (ADC->STATUS.bit.SYNCBUSY) {}
+  // ADC->CTRLA.bit.ENABLE = 0;
+  // while (ADC->STATUS.bit.SYNCBUSY) {}
   ADC->CTRLB.bit.PRESCALER = ADC_CTRLB_PRESCALER_DIV8_Val;
   ADC->CTRLA.reg |= ADC_CTRLA_ENABLE; // Enable the AD
-  while (ADC->STATUS.bit.SYNCBUSY) {}
+  // while (ADC->STATUS.bit.SYNCBUSY) {}
   // analogReference(AR_INTERNAL1V65);
   startTime = millis();
 }
@@ -23,27 +23,29 @@ void setup() {
 void loop() {
   float currentTime = (millis() - startTime) / 1000.0;
   float mV = ((analogRead(A0) - 400) * 1.2) / 4096.0 * 3300;
-  for (size_t i = 5; i >= 0; i--) {
+  for (int i = 5; i >= 0; i--) {
     if (mV > counts[i]) {
       counts[i + 6]++;
       break;
     }
   }
   Serial.println("**********************************");
-  Serial.print("Above ");
+  Serial.println("Total time: " + (String)currentTime);
+  Serial.print("Thresh ");
   for (size_t i = 0; i < 6; i++) {
-    Serial.print(counts[i] + " ");
+    Serial.print((String)counts[i] + " ");
   }
   Serial.println();
-  Serial.print("Counts");
+  Serial.print("Counts ");
   for (size_t i = 6; i < 12; i++) {
-    Serial.print(counts[i] + " ");
+    Serial.print((String)counts[i] + " ");
   }
   Serial.println();
-  Serial.print("CPS   ");
+  Serial.print("CPS    ");
   for (size_t i = 6; i < 12; i++) {
-    Serial.print(counts[i] / currentTime + " ");
+    Serial.print((String)(counts[i] / currentTime) + " ");
   }
+  Serial.println();
   // if (mV > 200)
     // Serial.println(mV);
   // Serial.println();
